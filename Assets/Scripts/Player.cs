b74fc29f70d8609575eb41608114320d74de40a1
kkppyau <kpyau@connect.ust.hk>
2019-10-09 10:49:24 +0800

    Add enemy chasing player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+            rb.AddForce(Vector3.forward * 20);
+        if (Input.GetKey(KeyCode.S))
+            rb.AddForce(Vector3.back * 20);
+        if (Input.GetKey(KeyCode.A))
+            rb.AddForce(Vector3.left * 20);
+        if (Input.GetKey(KeyCode.D))
+            rb.AddForce(Vector3.right * 20);
+    }
+}
