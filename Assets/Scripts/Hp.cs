edafff8487c6667492165382a2f2f6e46de563e5
ice21300 <43233189+ice21300@users.noreply.github.com>
2020-01-04 19:28:37 +0800

    ui of hp bar + some not workable scripts

---
+++ Assets/Scripts/Hp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Hp : MonoBehaviour
+{
+    private int maxHealth = 100;
+    private int currentHealth;
+
+    public event Action<float> On99 = delegate { };
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            currentHealth -= 10;
+            float currentHealthPct = (float) currentHealth / (float)maxHealth;
+            On99(currentHealthPct);
+        }
+            
+    }
+}
