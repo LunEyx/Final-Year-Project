b48e522bcad961dbcc441af25453ff6dd14cfe98
kkppyau <kpyau@connect.ust.hk>
2020-02-21 17:03:56 +0800

    Add fireball skill to player

---
+++ Assets/Scripts/SpellCaster.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellCaster : MonoBehaviour
+{
+    public GameObject prefab;
+    public float skillLife = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1)){
+            GameObject skill = Instantiate(prefab, transform.position + transform.forward * 2, transform.rotation);
+            skill.GetComponent<Rigidbody>().velocity = transform.forward * 40;
+            Destroy(skill, skillLife);
+        }
+    }
+}
