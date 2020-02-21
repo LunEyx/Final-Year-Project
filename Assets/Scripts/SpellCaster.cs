94b7db944b17915d2186282e041ac8bffc83e3d8
LunEyx <luneyxlab@gmail.com>
2020-02-21 18:01:35 +0800

    Removes unused function

--- Assets/Scripts/SpellCaster.cs
+++ Assets/Scripts/SpellCaster.cs
@@ -7,11 +7,6 @@ public class SpellCaster : MonoBehaviour
     public GameObject prefab;
     public float skillLife = 1f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
