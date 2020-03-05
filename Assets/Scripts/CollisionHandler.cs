c7e2ce9a060d1bdc5cbe8f7fe901034eec9672b7
kkppyau <kpyau@connect.ust.hk>
2020-03-05 13:47:50 +0800

    Add skill damage to destroy enemy

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class CollisionHandler : MonoBehaviour
 {
+    private int skillDamage = 10;
+
     private void OnCollisionEnter(Collision collision){
+        
         if (collision.gameObject.tag == "Enemy" || collision.gameObject.tag == "Obstacle"){
+
+            if (collision.gameObject.tag == "Enemy" && collision.gameObject.GetComponent<Enemy>().health > 0){
+                collision.gameObject.GetComponent<Enemy>().health -= skillDamage;
+                Debug.Log(collision.gameObject.GetComponent<Enemy>().health);
+            } else if (collision.gameObject.tag == "Enemy" && collision.gameObject.GetComponent<Enemy>().health <= 0){
+                Destroy(collision.gameObject);
+            }
+
             Destroy(gameObject);
         }
     }
