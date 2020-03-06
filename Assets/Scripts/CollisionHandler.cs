f1d08aec914954b18df2e4668bb7c5074856b797
z26753379x <43211022+z26753379x@users.noreply.github.com>
2020-03-06 22:04:04 +0800

    Finished the enemy hp bar

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -10,9 +10,9 @@ public class CollisionHandler : MonoBehaviour
         
         if (collision.gameObject.tag == "Enemy" || collision.gameObject.tag == "Obstacle"){
 
-            if (collision.gameObject.tag == "Enemy" && collision.gameObject.GetComponent<Enemy>().health > 0){
-                collision.gameObject.GetComponent<Enemy>().health -= skillDamage;
-            } else if (collision.gameObject.tag == "Enemy" && collision.gameObject.GetComponent<Enemy>().health <= 0){
+            if (collision.gameObject.tag == "Enemy" && collision.gameObject.GetComponent<Enemy>().hpSystem.get_hp() > 0){
+                collision.gameObject.GetComponent<Enemy>().hpSystem.damage(skillDamage);
+            } else if (collision.gameObject.tag == "Enemy" && collision.gameObject.GetComponent<Enemy>().hpSystem.get_hp() <= 0){
                 Destroy(collision.gameObject);
             }
 
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,23 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
 
     public GameObject player;
     private NavMeshAgent navmesh;
-    public int health = 100;
+    public HpSystem hpSystem = new HpSystem(100);
+    public Image hpBar;
+   
 
     // Start is called before the first frame update
     void Start()
     {
         navmesh = GetComponent<NavMeshAgent>();
+        hpBar.fillAmount = 0.5f;
     }
 
     // Update is called once per frame
     void Update()
     {
         navmesh.destination = player.transform.position;
+        hpBar.fillAmount = hpSystem.currentLifePercentage();
     }
 }
