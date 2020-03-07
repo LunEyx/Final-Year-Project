8ad1d3bbb126f951ccaa8daac17406693f4fd681
LunEyx <luneyxlab@gmail.com>
2020-03-07 22:29:56 +0800

    Removes non-related codes

--- Assets/Scripts/SightOfView.cs
+++ Assets/Scripts/SightOfView.cs
@@ -9,26 +9,19 @@ public class SightOfView : MonoBehaviour
     public float viewAngle;
 
     public LayerMask targetMask;
-    public LayerMask obstaclemask;
+    public LayerMask obstacleMask;
 
-    public int edgeResolveIterations;
-    public float edgeDstThreshold;
-
-    // [HideInInspector]
+    [HideInInspector]
     public List<Transform> visibleTargets = new List<Transform>();
 
-    void Start()
+    private void Update()
     {
-        StartCoroutine("FindTargetsWithDelay", .2f);
+        FindTargets();
     }
-    
-    IEnumerator FindTargetsWithDelay(float delay)
+
+    void FindTargets()
     {
-        while (true)
-        {
-            yield return new WaitForSeconds(delay);
-            FindVisibleTarget();
-        }
+        FindVisibleTarget();
     }
 
     void FindVisibleTarget()
@@ -39,10 +32,10 @@ public class SightOfView : MonoBehaviour
         {
             Transform target = targetsInViewRadius[i].transform;
             Vector3 dirToTarget = (target.position - transform.position).normalized;
-            if (Vector3.Angle(transform.forward, dirToTarget) < viewAngle / 2) {
+            if (Vector3.Angle(transform.forward, dirToTarget) < viewAngle / 2)
+            {
                 float dstToTarget = Vector3.Distance(transform.position, target.position);
-
-                if (!Physics.Raycast(transform.position, dirToTarget, dstToTarget, obstaclemask))
+                if (!Physics.Raycast(transform.position, dirToTarget, dstToTarget, obstacleMask))
                 {
                     visibleTargets.Add(target);
                 }
@@ -50,81 +43,12 @@ public class SightOfView : MonoBehaviour
         }
     }
 
-    EdgeInfo FindEdge(ViewCastInfo minViewCast, ViewCastInfo maxViewCast)
+    public Vector3 DirFromAngle(float angleDeg, bool angleIsGlobal)
     {
-        float minAngle = minViewCast.angle;
-        float maxAngle = maxViewCast.angle;
-        Vector3 minPoint = Vector3.zero;
-        Vector3 maxPoint = Vector3.zero;
-         
-        for (int i = 0; i < edgeResolveIterations; i++)
-        {
-            float angle = (minAngle + maxAngle) / 2;
-            ViewCastInfo newViewCast = ViewCast(angle);
-
-            bool edgeDstThresholdExceeded = Mathf.Abs(minViewCast.dst - newViewCast.dst) > edgeDstThreshold;
-            if (newViewCast.hit == minViewCast.hit && !edgeDstThresholdExceeded)
-            {
-                minAngle = angle;
-                maxPoint = newViewCast.point;
-            }
-            else
-            {
-                maxAngle = angle;
-                minPoint = newViewCast.point;
-            }
-        }
-        return new EdgeInfo(minPoint, maxPoint);
-    }
-
-    ViewCastInfo ViewCast(float globalAngle)
-    {
-        Vector3 dir = DirFromAngle(globalAngle, true);
-        RaycastHit hit;
-
-        if (Physics.Raycast(transform.position, dir, out hit, viewRadius, obstaclemask))
-        {
-            return new ViewCastInfo(true, hit.point, hit.distance, globalAngle);
-        }
-        else
-        {
-            return new ViewCastInfo(false, transform.position + dir * viewRadius, viewRadius, globalAngle);
-        }
-    }
-
-    public Vector3 DirFromAngle(float angleDeg, bool angleIsGlobal) {
         if (!angleIsGlobal)
         {
             angleDeg += transform.eulerAngles.y;
         }
         return new Vector3(Mathf.Sin(angleDeg * Mathf.Deg2Rad), 0, Mathf.Cos(angleDeg * Mathf.Deg2Rad));
-    }
-
-    public struct ViewCastInfo
-    {
-        public bool hit;
-        public Vector3 point;
-        public float dst;
-        public float angle;
-
-        public ViewCastInfo(bool _hit, Vector3 _point, float _dst, float _angle)
-        {
-            hit = _hit;
-            point = _point;
-            dst = _dst;
-            angle = _angle;
-        }
-    }
-
-    public struct EdgeInfo
-    {
-        public Vector3 pointA;
-        public Vector3 pointB;
-
-        public EdgeInfo(Vector3 _pointA, Vector3 _pointB)
-        {
-            pointA = _pointA;
-            pointB = _pointB;
-        }
     }
 }
