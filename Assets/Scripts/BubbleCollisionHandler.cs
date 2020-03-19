11b01d6386157fdcdee5ca7f3417c728f91c4ece
z26753379x <43211022+z26753379x@users.noreply.github.com>
2020-03-19 17:21:54 +0800

    Finished bubble skill

---
+++ Assets/Scripts/BubbleCollisionHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleCollisionHandler : MonoBehaviour
+{
+    private int skillDamage = 5;
+    private float bubbleDuration = 5f;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.tag == "Enemy" || collision.gameObject.tag == "Obstacle")
+        {
+
+            if (collision.gameObject.tag == "Enemy" && collision.gameObject.GetComponent<Enemy>().hpSystem.get_hp() > 0)
+            {
+                collision.gameObject.GetComponent<Enemy>().hpSystem.damage(skillDamage);
+                collision.gameObject.GetComponent<Enemy>().bubbled = true;
+                collision.gameObject.GetComponent<Enemy>().bubbleDuration = bubbleDuration;
+            }
+            if (collision.gameObject.tag == "Enemy" && collision.gameObject.GetComponent<Enemy>().hpSystem.get_hp() <= 0)
+            {
+                Destroy(collision.gameObject);
+            }
+
+            Destroy(gameObject);
+        }
+    }
+
+}
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,6 +7,7 @@ using UnityEngine.UI;
 public class Enemy : MonoBehaviour
 {
     public GameObject player;
+    public GameObject bubblePrefab;
     public GameObject skillPrefab;
     private SightOfView sightOfView;
     private NavMeshAgent navmesh;
@@ -16,6 +17,11 @@ public class Enemy : MonoBehaviour
     private float attackCooldownTimer;
     public HpSystem hpSystem = new HpSystem(100);
     public Image hpBar;
+    public bool bubbled = false;
+    public float bubbleDuration = 5f;
+    private bool bubbleGenerated = false;
+    private GameObject tempBubble;
+
 
     // Start is called before the first frame update
     void Start()
@@ -28,33 +34,61 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (attackCooldownTimer > 0)
+        if (!bubbled)
         {
-            attackCooldownTimer -= Time.deltaTime;
-        }
-        else
-        {
-            attackCooldownTimer = 0;
-        }
-
-        if (sightOfView.visibleTargets.Count == 0)
-        {
-            navmesh.destination = player.transform.position;
-        }
-        else
-        {
-            navmesh.destination = navmesh.transform.position;
-            Vector3 targetPos = sightOfView.visibleTargets[0].position;
-            Quaternion targetRotation = Quaternion.LookRotation(targetPos - transform.position);
-            navmesh.transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 10f * Time.deltaTime);
-            if (attackCooldownTimer == 0)
+            if (attackCooldownTimer > 0)
             {
-                Attack();
-                attackCooldownTimer = Random.Range(minAttackCooldown, maxAttackCooldown);
+                attackCooldownTimer -= Time.deltaTime;
+            }
+            else
+            {
+                attackCooldownTimer = 0;
+            }
+
+            if (sightOfView.visibleTargets.Count == 0)
+            {
+                navmesh.destination = player.transform.position;
+            }
+            else
+            {
+                navmesh.destination = navmesh.transform.position;
+                Vector3 targetPos = sightOfView.visibleTargets[0].position;
+                Quaternion targetRotation = Quaternion.LookRotation(targetPos - transform.position);
+                navmesh.transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 10f * Time.deltaTime);
+                if (attackCooldownTimer == 0)
+                {
+                    Attack();
+                    attackCooldownTimer = Random.Range(minAttackCooldown, maxAttackCooldown);
+                }
             }
         }
+        else
+        {
+            if (!bubbleGenerated)
+            {
+                tempBubble = Instantiate(bubblePrefab, this.transform.position, this.transform.rotation);
+                bubbleGenerated = true;
+            }
+                
+            navmesh.destination = this.transform.position;
+            this.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            StartCoroutine(StopMoves());    
+            
+        }
+
         hpBar.fillAmount = hpSystem.currentLifePercentage();
+
+        
     }
+
+    IEnumerator StopMoves()
+    {
+        yield return new WaitForSeconds(bubbleDuration);
+        bubbled = false;
+        bubbleGenerated = false;
+        Destroy(tempBubble);
+    }
+
 
     void Attack()
     {
