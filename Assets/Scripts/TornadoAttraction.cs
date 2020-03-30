ce795fa317c5e17159dc61947904c509aa446f67
kkppyau <kpyau@connect.ust.hk>
2020-03-30 17:47:45 +0800

    Add tornado

---
+++ Assets/Scripts/TornadoAttraction.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TornadoAttraction : MonoBehaviour
+{
+    public Transform particleCenter;
+    public float pullForce;
+    public float refreshRate;
+
+    private void OnTriggerEnter(Collider obj){
+        StartCoroutine(pullObject(obj, true));
+    }
+
+    private void OnTriggerExit(Collider obj){
+        StartCoroutine(pullObject(obj, false));
+    }
+
+    IEnumerator pullObject(Collider obj, bool pullable){
+        if (pullable){
+            Vector3 pullDirection = particleCenter.position - obj.transform.position;
+            obj.GetComponent<Rigidbody>().AddForce(pullDirection.normalized * pullForce * Time.deltaTime);
+            yield return refreshRate;
+            StartCoroutine(pullObject(obj, pullable));
+        }
+    }
+}
