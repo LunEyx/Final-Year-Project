d161e0a88bd9d40a801928b0ad7cb8d131ca945d
z26753379x <43211022+z26753379x@users.noreply.github.com>
2020-04-16 15:04:18 +0800

    Finished basic layout

---
+++ Assets/Scripts/ShopTriggerCollider.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopTriggerCollider : MonoBehaviour
+{
+    [SerializeField] private ShopUIController shop_UI;
+    private void OnTriggerEnter(Collider collider)
+    {
+        Player player = collider.GetComponent<Player>();
+        if (player != null)
+        {
+            shop_UI.Show(player);
+        }
+    }
+
+    private void OnTriggerExit(Collider collider)
+    {
+        Player player = collider.GetComponent<Player>();
+        if (player != null)
+        {
+            shop_UI.Hide();
+        }
+    }
+}
---
+++ Assets/Scripts/ShopUIController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopUIController : MonoBehaviour
+{
+    private Transform shopUI;
+    private Transform shopItemTemplate;
+    private Player player;
+
+    private void Awake()
+    {
+        shopUI = transform.Find("Shop_UI");
+        shopItemTemplate = shopUI.Find("Shop_Item_Container");
+        shopItemTemplate.gameObject.SetActive(false);
+    }
+
+    public void Show(Player player)
+    {
+        this.player = player;
+        gameObject.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
